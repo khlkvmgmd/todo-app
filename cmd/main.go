8fbcb2627469f78c87f8b394c57161385a7c00fa
khalikov <aliev-ali-200201@mail.ru>
2023-08-30 15:43:23 +0300

    create file server.go

---
+++ cmd/main.go
@@ -0,0 +1,10 @@
+package main
+
+// import "https://github.com/khlkvmgmd/todo-app"
+
+// func main() {
+// 	srv := new(todo.Server)
+// 	if err := srv.Run("8000"); err != nil {
+// 		log.Fatalf()
+// 	}
+// }
